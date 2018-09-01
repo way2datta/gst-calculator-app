ecd14983d167978cce36eea5f67415e9b33d2d8f
Dattatray Kale <way2datta@gmail.com>
2018-09-01 13:03:52 +0530

    Category and gst rate mapping, item and category mapping added.

--- gst-calculator.js
+++ gst-calculator.js
@@ -1,15 +1,30 @@
+function Item () {
+    this.name = "";
+    this.quantity = 0;
+    this.initialPrice = 0.0;
 
-const itemName = prompt("Enter the item");
-const itemQuantity = parseInt(prompt("Enter quantity of the item"));
-const itemInitialPrice = parseFloat(prompt("Enter initial price of the item"));
+    this.acceptInput = function () {
+        this.name = prompt("Enter the item");
+        this.quantity = parseInt(prompt("Enter quantity of the item"));
+        this.initialPrice = parseFloat(prompt("Enter initial price of the item"));
+    }
+};
 
-console.log("Item name: "+itemName);
-console.log("Item quantity: "+itemQuantity);
-console.log("Item initial price: "+itemInitialPrice);
+const categoryGstRateMapping = {"Food-grains": 0, "Furniture": 5, "Electronics":18};
+const itemCategoryMapping = {"Rice": "Food-grains", "TV": "Electronics", "Sofa": "Furniture"};
 
 
-const itemsPricesWithoutGst = itemQuantity * itemInitialPrice;
+var item = new Item();
+item.acceptInput();
+console.log("Item name: "+item.name);
+console.log("Item quantity: "+item.quantity);
+console.log("Item initial price: "+item.initialPrice);
+
+var categoryName = itemCategoryMapping[item.name];
+
+const itemsPricesWithoutGst = item.quantity * item.initialPrice;
 const gstOnSellingItem = itemsPricesWithoutGst * 0;
 const sellingPrice = itemsPricesWithoutGst + gstOnSellingItem;
 
+console.log(categoryName);
 console.log("Final selling price: " + sellingPrice);
