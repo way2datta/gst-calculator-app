e88e428dce16c65877571958aa708fad3cd920a6
Dattatray Kale <way2datta@gmail.com>
2018-09-02 18:09:59 +0530

    Gst provider class added

--- gst-calculator.js
+++ gst-calculator.js
@@ -1,16 +1,26 @@
-function Item () {
-    this.name = "";
-    this.quantity = 0;
-    this.initialPrice = 0.0;
-
-    this.acceptInput = function () {
+class Item {
+    constructor() {
+        this.name = "";
+        this.quantity = 0;
+        this.initialPrice = 0.0;
+    }
+    acceptInput() {
         this.name = prompt("Enter the item");
         this.quantity = parseInt(prompt("Enter quantity of the item"));
         this.initialPrice = parseFloat(prompt("Enter initial price of the item"));
-    }
+    };
 };
 
-const categoryGstRateMapping = {"Food-grains": 0, "Furniture": 5, "Electronics":18};
+class GstRateProvider {
+    constructor() {
+        this.categoryGstRateMapping = {"Food-grains": 0, "Furniture": 5, "Electronics":18};
+    }
+    getGSTRateFor(categoryName) {
+        return this.categoryGstRateMapping[categoryName];
+    }
+}
+
+
 const itemCategoryMapping = {"Rice": "Food-grains", "TV": "Electronics", "Sofa": "Furniture"};
 
 
@@ -22,7 +32,7 @@ console.log("Item quantity: "+item.quantity);
 console.log("Item initial price: "+item.initialPrice);
 
 var categoryName = itemCategoryMapping[item.name];
-var gstRateApplicableToItem = categoryGstRateMapping[categoryName];
+var gstRateApplicableToItem = new GstRateProvider().getGSTRateFor(categoryName);
 
 const itemsPricesWithoutGst = item.quantity * item.initialPrice;
 const gstOnSellingItem = itemsPricesWithoutGst * gstRateApplicableToItem / 100;
